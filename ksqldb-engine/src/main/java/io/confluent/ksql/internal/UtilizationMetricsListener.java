b3d7505d8fd85bd6f1a87a798af31b88cefa9f77
Leah Thomas <lthomas@confluent.io>
2021-06-28 13:19:56 -0500

    initial disk util by node

--- ksqldb-engine/src/main/java/io/confluent/ksql/internal/UtilizationMetricsListener.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/internal/UtilizationMetricsListener.java
@@ -3,29 +3,46 @@ package io.confluent.ksql.internal;
 import io.confluent.ksql.engine.QueryEventListener;
 import io.confluent.ksql.metastore.MetaStore;
 import io.confluent.ksql.services.ServiceContext;
+import io.confluent.ksql.util.KsqlConfig;
 import io.confluent.ksql.util.QueryMetadata;
+import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UtilizationMetricsListener implements Runnable, QueryEventListener {
 
     private final List<KafkaStreams> kafkaStreams;
+    private final List<File> streamsDirectories;
     private final Logger logger = LoggerFactory.getLogger(UtilizationMetricsListener.class);
-    private final List<String> metrics;
     private final Time time;
-    private long lastSampleTime;
+    private File baseDir;
 
-    public UtilizationMetricsListener(){
+    public UtilizationMetricsListener(final KsqlConfig config){
         this.kafkaStreams = new ArrayList<>();
-        this.metrics = new LinkedList<>();
+        this.streamsDirectories = new ArrayList<>();
+        this.baseDir = new File(config.getKsqlStreamConfigProps()
+            .getOrDefault(
+                StreamsConfig.STATE_DIR_CONFIG,
+                StreamsConfig.configDef().defaultValues().get(StreamsConfig.STATE_DIR_CONFIG))
+            .toString());
         time = Time.SYSTEM;
-        lastSampleTime = time.milliseconds();
+    }
+
+    // for testing
+    public UtilizationMetricsListener(final List<KafkaStreams> streams, final Time time, final long lastSample) {
+        this.kafkaStreams = streams;
+        this.streamsDirectories = new ArrayList<>();
+        this.baseDir = new File("");
+        this.time = time;
     }
 
     @Override
@@ -34,6 +51,7 @@ public class UtilizationMetricsListener implements Runnable, QueryEventListener 
             final MetaStore metaStore,
             final QueryMetadata queryMetadata) {
         kafkaStreams.add(queryMetadata.getKafkaStreams());
+        streamsDirectories.add(new File(baseDir, queryMetadata.getQueryApplicationId()));
     }
 
     @Override
@@ -45,8 +63,35 @@ public class UtilizationMetricsListener implements Runnable, QueryEventListener 
     @Override
     public void run() {
         logger.info("Reporting Observability Metrics");
-        final Long currentTime = time.milliseconds();
-        // here is where we would report metrics
-        lastSampleTime = currentTime;
+        logger.info("Reporting CSU thread level metrics");
+        nodeDiskUsage();
+        taskDiskUsage();
+    }
+
+    private double percentage(final double b, final double w) {
+        return Math.round((b / w) * 100);
+    }
+
+    final void nodeDiskUsage() {
+        for (File f : streamsDirectories) {
+            final long freeSpace = f.getFreeSpace();
+            final long totalSpace = f.getTotalSpace();
+            final double percFree = percentage((double) freeSpace, (double) totalSpace);
+            logger.info("The disk usage for {} is {}", f.getName(), freeSpace);
+            logger.info("The % disk space free for {} is {}%", f.getName(), percFree);
+        }
+    }
+
+    final void taskDiskUsage() {
+        for (KafkaStreams streams : kafkaStreams) {
+            logger.info("Recording task level disk usage for {}", streams);
+            final List<Metric> fileSizePerTask = streams.metrics().values().stream()
+                .filter(m -> m.metricName().name().equals("total-sst-files-size"))
+                .collect(Collectors.toList());
+            for (Metric m : fileSizePerTask) {
+                final BigInteger usage = (BigInteger) m.metricValue();
+                logger.info("Disk usage for {} is {}", m.metricName().tags().getOrDefault("task-id", ""), usage);
+            }
+        }
     }
 }
